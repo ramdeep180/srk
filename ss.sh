5edb7fd77f9d2e28e1a12a7af60d3d3a33b69fbe
ramdeep <ramdeep180@gmail.com>
2020-05-19 21:39:18 +0530

    comments added

--- ss.sh
+++ ss.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
+echo "_______running metadata"
 PH=$(curl -sL http://169.254.169.254/latest/meta-data/public-hostname)
 PSG=$(curl -sL http://169.254.169.254/latest/meta-data/security-groups)
 PIP=$(curl -sL http://169.254.169.254/latest/meta-data/public-ipv4)
 TYPE=$(curl -sL http://169.254.169.254/latest/meta-data/instance-type)
 ID=$(curl -sL http://169.254.169.254/latest/meta-data/instance-id)
-
+echo "completed....!!!!!"
 echo "The public host name is $PH"
 echo "The public ip is $PIP"
 echo "$1 is PSG"
