d776081b0e31eb5372d051549f58557557ad8d3c
root <root@ip-172-31-83-103.ec2.internal>
2020-05-19 15:55:37 +0000

    Updated p name and p ip information

--- ss.sh
+++ ss.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-PH=$(curl -sL http://168.254.169.254/latest/meta-data/public-hostname)
-PSG=$(curl -sL http://168.254.169.254/latest/meta-data/security-groups)
-PIP=$(curl -sL http://168.254.169.254/latest/meta-data/public-ipv4)
-TYPE=$(curl -sL http://168.254.169.254/latest/meta-data/instance-type)
-ID=$(curl -sL http://168.254.169.254/latest/meta-data/instance-id)
+PH=$(curl -sL http://169.254.169.254/latest/meta-data/public-hostname)
+PSG=$(curl -sL http://169.254.169.254/latest/meta-data/security-groups)
+PIP=$(curl -sL http://169.254.169.254/latest/meta-data/public-ipv4)
+TYPE=$(curl -sL http://169.254.169.254/latest/meta-data/instance-type)
+ID=$(curl -sL http://169.254.169.254/latest/meta-data/instance-id)
 
-echo "$PH"
-echo "$PIP"
+echo "The public host name is $PH"
+echo "The public ip is $PIP"
 echo "$PSG"
 echo "$TYPE"
 echo "$ID" 
