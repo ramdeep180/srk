621908c20e29e3dac19a7d489b24454156d94f0e
root <root@ip-172-31-83-103.ec2.internal>
2020-05-19 15:53:04 +0000

    Base EC2 shell script

---
+++ ss.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+PH=$(curl -sL http://168.254.169.254/latest/meta-data/public-hostname)
+PSG=$(curl -sL http://168.254.169.254/latest/meta-data/security-groups)
+PIP=$(curl -sL http://168.254.169.254/latest/meta-data/public-ipv4)
+TYPE=$(curl -sL http://168.254.169.254/latest/meta-data/instance-type)
+ID=$(curl -sL http://168.254.169.254/latest/meta-data/instance-id)
+
+echo "$PH"
+echo "$PIP"
+echo "$PSG"
+echo "$TYPE"
+echo "$ID" 
+
